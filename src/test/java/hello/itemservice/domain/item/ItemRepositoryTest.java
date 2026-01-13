77eda1b6410ca4e92b21aee6ac793f54ce690537
Yoo <yooyoo9191@gmail.com>
2026-01-13 17:46:46 +0900

    feat: item, respository 구현

---
+++ src/test/java/hello/itemservice/domain/item/ItemRepositoryTest.java
@@ -0,0 +1,54 @@
+package hello.itemservice.domain.item;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
+class ItemRepositoryTest {
+
+    ItemRepository itemRepository = new ItemRepository();
+
+    @AfterEach
+    void afterEach(){
+        itemRepository.clearStore();
+    }
+
+    @Test
+    void save(){
+        Item item = new Item("itemA", 10000, 10);
+        Item savedItem = itemRepository.save(item);
+        Item findItem = itemRepository.findById(item.getId());
+
+        assertThat(findItem).isEqualTo(savedItem);
+
+    }
+    @Test
+    void findAll(){
+        Item item1 = new Item("itemA", 10000, 10);
+        Item item2 = new Item("itemB", 20000, 30);
+        itemRepository.save(item1);
+        itemRepository.save(item2);
+
+        List<Item> result = itemRepository.findAll();
+
+        assertThat(result.size()).isEqualTo(2);
+        assertThat(result).contains(item1, item2);
+    }
+    @Test
+    void updateItem(){
+        Item item = new Item("itemA", 10000, 10);
+        Item savedItem = itemRepository.save(item);
+
+        Long id = item.getId();
+        Item updateParam = new Item("item2", 20000, 30);
+        itemRepository.update(id, updateParam);
+        Item findItem = itemRepository.findById(id);
+
+        assertThat(findItem.getItemName()).isEqualTo(savedItem.getItemName());
+
+    }
+}
